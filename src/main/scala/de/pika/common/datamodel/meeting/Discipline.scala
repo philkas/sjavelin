225c223cddf6bef59c5e24680e41abac7e5625ae
Philipp Kastner <philipp.kastner@gmx.de>
2015-10-22 22:12:36 +0200

    defined another discipline implementation

--- src/main/scala/de/pika/common/datamodel/meeting/Discipline.scala
+++ src/main/scala/de/pika/common/datamodel/meeting/Discipline.scala
@@ -5,25 +5,37 @@ import de.pika.common.datamodel.measure.Meter
 /**
  * Created by pika on 22.10.15.
  */
-sealed abstract class Discipline {
-  def name: String
-}
 
-sealed abstract class RunDiscipline extends Discipline {
-  def distance: Meter
-}
+object Disciplines {
 
-sealed case class SprintDiscipline(val name: String, val distance: Meter) extends RunDiscipline
-sealed case class LongrunDiscipline(val name: String, val distance: Meter) extends RunDiscipline
+  sealed abstract class Discipline extends Ordered[Discipline] {
+    def name: String
+    def id: Int
 
-sealed abstract class TechnicalDiscipline extends Discipline
+    def compare(that: Discipline) = this.id - that.id
+  }
 
-sealed case class ThrowingDiscipline (val name: String) extends Discipline
-sealed case class VerticalJumpDiscipline(val name: String) extends Discipline
-sealed case class HorizontalJumpDiscipline(val name: String) extends Discipline
+  sealed abstract class RunDiscipline extends Discipline {
+    def distance: Meter
+  }
+  sealed abstract class SprintDiscipline(val id: Int, val name: String, val distance: Meter) extends RunDiscipline
+  sealed abstract class LongrunDiscipline(val id: Int, val name: String, val distance: Meter) extends RunDiscipline
 
-object Discipline {
-  lazy val _50m = SprintDiscipline("50m", Meter(50.0))
-  lazy val _100m = SprintDiscipline("100m", Meter(100.0))
-  lazy val _200m = SprintDiscipline("200m", Meter(200.0))
+  sealed abstract class TechnicalDiscipline extends Discipline
+  sealed abstract class ThrowingDiscipline(val id: Int, val name: String) extends Discipline
+  sealed abstract class VerticalJumpDiscipline(val id: Int, val name: String) extends Discipline
+  sealed abstract class HorizontalJumpDiscipline(val id: Int, val name: String) extends Discipline
+
+  case object D_LONGJUMP extends HorizontalJumpDiscipline(1, "Longjump")
+  case object D_HIGHJUMP extends VerticalJumpDiscipline(2, "Highjump")
+  case object D_DISCUS extends ThrowingDiscipline(3, "Discus")
+  case object D_100M extends SprintDiscipline(4, "100m", Meter(100.0))
+  case object D_1000M extends LongrunDiscipline(5, "1000m", Meter(1000.0))
+
+  val disciplines = Seq(D_1000M, D_100M, D_LONGJUMP, D_DISCUS, D_HIGHJUMP)
+
+  def runDisciplines() : Seq[Discipline] = {
+    disciplines.filter(_.isInstanceOf[RunDiscipline])
+  }
+
 }
