cb26ff886ad7adc7b2ede31d34bcbf47aac23711
Philipp Kastner <philipp.kastner@gmx.de>
2015-10-22 18:30:02 +0200

    added Discipline hierarchie

---
+++ src/main/scala/de/pika/common/datamodel/measure/Meter.scala
@@ -0,0 +1,9 @@
+package de.pika.common.datamodel.measure
+
+/**
+ * Created by pika on 22.10.15.
+ */
+class Meter(val meter: Double) extends AnyVal {}
+
+class CentiMeter(val centiMeter: Int) extends AnyVal {}
+
---
+++ src/main/scala/de/pika/common/datamodel/meeting/Discipline.scala
@@ -0,0 +1,23 @@
+package de.pika.common.datamodel.meeting
+
+import de.pika.common.datamodel.measure.Meter
+
+/**
+ * Created by pika on 22.10.15.
+ */
+sealed abstract class Discipline {
+  def name: String
+}
+
+sealed abstract class RunDiscipline extends Discipline {
+  def distance: Meter
+}
+
+sealed case class SprintDiscipline(val name: String, val distance: Meter) extends RunDiscipline
+sealed case class LongrunDiscipline(val name: String, val distance: Meter) extends RunDiscipline
+
+sealed abstract class TechnicalDiscipline extends Discipline
+
+sealed case class ThrowingDiscipline (val name: String) extends Discipline
+sealed case class VerticalJumpDiscipline(val name: String) extends Discipline
+sealed case class HorizontalJumpDiscipline(val name: String) extends Discipline
