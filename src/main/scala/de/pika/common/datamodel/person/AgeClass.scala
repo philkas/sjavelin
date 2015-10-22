3a38e1e33496f48b41f5c6a6c0cd0e836875df31
Philipp Kastner <philipp.kastner@gmx.de>
2015-10-23 00:02:04 +0200

    implemented AgeClass

--- src/main/scala/de/pika/common/datamodel/person/AgeClass.scala
+++ src/main/scala/de/pika/common/datamodel/person/AgeClass.scala
@@ -1,8 +1,18 @@
 package de.pika.common.datamodel.person
 
+import de.pika.common.datamodel.person.Gender.Gender
+
 /**
  * Created by pika on 22.10.15.
  */
-class AgeClass {
+sealed case class AgeClass(val description: String, val shortDesc: String, val range: Range, val gender: Gender) {
+  def contains(age: Int): Boolean = {
+    range.contains(age)
+  }
+}
 
+object AgeClass {
+  def fromAge(classes: Seq[AgeClass], age: Int): Option[AgeClass] = {
+    classes.find(_.contains(age))
+  }
 }
