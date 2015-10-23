11e4140e0ece244e59e722411a70ef21084394bc
Philipp Kastner <philipp.kastner@gmx.de>
2015-10-23 20:24:04 +0200

    Competitor to Person

--- src/main/scala/de/pika/common/datamodel/person/Person.scala
+++ src/main/scala/de/pika/common/datamodel/person/Person.scala
@@ -1,7 +1,25 @@
 package de.pika.common.datamodel.person
 
+import java.time.LocalDate
+
+import de.pika.common.datamodel.person.Gender.Gender
+
 /**
  * Created by pika on 22.10.15.
  */
-abstract class Person(firstName: String, lastName: String)
-case class CompPerson(val firstName: String, val lastName: String) extends Person(firstName, lastName)
+class Person(val firstName: String, val lastName: String)
+
+abstract class Competitor {
+  def name: String
+  def gender: Gender
+  def birthyear: Int
+  def age: Int = {
+    LocalDate.now().getYear - birthyear
+  }
+}
+
+class PersonCompetitor(val person: Person, val gender: Gender, val birthyear: Int) extends Competitor {
+  def name: String = {
+    person.firstName concat  person.lastName
+  }
+}
